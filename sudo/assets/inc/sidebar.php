387027918e435a369cee9d9d35c3ae5f581a54db
admin <chiencn05@gmail.com>
2023-06-07 08:37:18 +0700

    update

---
+++ sudo/assets/inc/sidebar.php
@@ -0,0 +1,238 @@
+<aside id="sidebar_main">
+        
+        <div class="sidebar_main_header">
+            <div class="sidebar_logo">
+                <a href="pages_sudo_dashboard.php" class="sSidebar_hide sidebar_logo_large">
+                    <img class="logo_regular" src="assets/img/logo-1.png" alt="" height="60" width="71"/>
+                    <img class="logo_light" src="assets/img/logo-1.png" alt="" height="60" width="71"/>
+                </a>
+                <a href="pages_sudo_dashboard.php" class="sSidebar_show sidebar_logo_small">
+                    <img class="logo_regular" src="assets/img/logo-1.png" alt="" height="50" width="32"/>
+                    <img class="logo_light" src="assets/img/logo-1.png" alt="" height="50" width="32"/>
+                </a>
+            </div>
+        </div>
+        
+        <div class="menu_section">
+            <ul>
+                <!--Dashboard-->
+                <li title="Dashboard">
+                    <a href="pages_sudo_dashboard.php">
+                        <span class="menu_icon"><i class="material-icons">&#xE871;</i></span>
+                        <span class="menu_title">Dashboard</span>
+                    </a>
+                    
+                </li>
+                <!--
+                <li title="Mailbox">
+                    <a href="page_mailbox.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE158;</i></span>
+                        <span class="menu_title">Mailbox</span>
+                    </a>
+                    
+                </li>
+                <li title="Invoices">
+                    <a href="page_invoices.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE53E;</i></span>
+                        <span class="menu_title">Invoices</span>
+                    </a>
+                    
+                </li>
+                -->
+
+                <!--Librarians-->
+                <li title="Librarians">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">people</i></span>
+                        <span class="menu_title">Thủ Thư</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_librarian.php">Thêm</a></li>
+                        <li><a href="pages_sudo_manage_librarians.php">Quản lý</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Students-->
+                <li title="Students">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE87C;</i></span>
+                        <span class="menu_title">Sinh Viên</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_student.php">Thêm</a></li>
+                        <li><a href="pages_sudo_manage_student.php">Quản lý</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--
+                <li title="Scrum Board">
+                    <a href="page_scrum_board.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE85C;</i></span>
+                        <span class="menu_title">Scrum Board</span>
+                    </a>
+                    
+                </li>
+                <li title="Snippets">
+                    <a href="page_snippets.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE86F;</i></span>
+                        <span class="menu_title">Snippets</span>
+                    </a>
+                    
+                </li>
+                <li title="User Profile">
+                    <a href="page_user_profile.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE87C;</i></span>
+                        <span class="menu_title">User Profile</span>
+                    </a>
+                    
+                </li>
+                <li title="Sticky Notes">
+                    <a href="page_sticky_notes.html">
+                        <span class="menu_icon"><i class="material-icons">&#xE8F0;</i></span>
+                        <span class="menu_title">Sticky Notes</span>
+                    </a>
+                    
+                </li>
+                -->
+
+                <!--Book Inventory-->
+                <li title="Books Inventory">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8D2;</i></span>
+                        <span class="menu_title">Kho sách</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_book_category.php">Thêm thể loại sách</a></li>
+                        <li><a href="pages_sudo_manage_categories.php">Quản lý thể loại</a></li>
+                        <li><a href="pages_sudo_new_book.php">Thêm sách</a></li>
+                        <li><a href="pages_sudo_manage_books.php">Quản lý sách</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Library Operations-->
+                <li title="Library Operations">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8C0;</i></span>
+                        <span class="menu_title">Hoạt động của thu viện</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_library_book_borrow_operation.php">Mượn sách</a></li>
+                        <li><a href="pages_sudo_new_library_book_return_operation.php">Trả sách</a></li>
+                        <li><a href="pages_sudo_library_operations_lost_book.php">Sách bị mất</a></li>
+                        <li><a href="pages_sudo_library_operations_damanged_book.php">Sách bị hỏng</a></li>
+                        <li><a href="pages_sudo_manage_library_operations.php">Quản lý</a></li>
+                        
+                    </ul>
+                
+                </li>
+
+                <!--Library Finances-->
+                <li title="Library Finances">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">money</i></span>
+                        <span class="menu_title">Tài chính thư viện</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_penalty.php">Thêm tỷ lệ phạt</a></li>
+                        <li><a href="pages_sudo_manage_penalty.php">Quản lý mức phạt</a></li>
+                        <li><a href="pages_sudo_manage_finances.php">Quản lý tài chính</a></li>
+                    </ul>
+                
+                </li>
+
+                <!--Subscriptions-->
+                <!-- <li title="Subscriptions">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8F1;</i></span>
+                        <span class="menu_title">Subcriptions</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_subscription.php">Add</a></li>
+                        <li><a href="pages_sudo_manage_subscriptions.php">Manage</a></li>
+                    </ul>
+                
+                </li> -->
+
+                <!--Recomendations-->
+                <!-- <li title="Recomendantions">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE1BD;</i></span>
+                        <span class="menu_title">Recomendations</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_recomendation.php">Add</a></li>
+                        <li><a href="pages_sudo_manage_reccomendations.php">Manage</a></li>
+                    </ul>
+                
+                </li> -->
+
+                <!--Audits-->
+                <li title="Audits">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE87B;</i></span>
+                        <span class="menu_title">Kiểm toán</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_audit_book_categories.php">Danh mục sách</a></li>
+                        <li><a href="pages_sudo_audit_books_records.php">Hồ sơ sách</a></li>
+                        <li><a href="pages_sudo_audit_library_operations.php">Hoạt động thư viện</a></li>
+                        <li><a href="pages_sudo_audit_subscriptions.php">Bản ghi đăng ký</a></li>
+                        <li><a href="pages_sudo_audit_fines_payments.php">Hồ sơ thanh toán tiền</a></li>
+
+                        
+                    </ul>
+                
+                </li>
+
+                <!--Library Store--
+                <li title="iLibrary Store">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">&#xE8CB;</i></span>
+                        <span class="menu_title">iLibrary Store</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_new_store_item.php">New Product</a></li>
+                        <li><a href="pages_sudo_manage_store_items.php">Manage Products</a></li>
+                        <li><a href="pages_sudo_manage_orders.php">Manage Orders</a></li>
+                        <li><a href="pages_sudo_manage_payments.php">Manage Payments</a></li>
+                        <li><a href="pages_sudo_ship_purchasesed_products.php">Ship Products</a></li>
+                    </ul>
+                
+                </li>
+                -->
+                <!-- Mail
+                This feature will be implemented on future versions               
+             
+               
+                <li title="iLibrary Store">
+                    <a href="#">
+                    <span class="menu_icon"><i class="material-icons">&#xE158;</i></span>
+                        <span class="menu_title">Mailbox</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_mail_students.php">Student Mail</a></li>
+                        <li><a href="pages_sudo_mail_staff.php">Staff Mail</a></li>
+                        <li><a href="pages_sudo_manage_inbox.php">Manage Mail</a></li>
+                        <li><a href="pages_sudo_manage_sendmails.php">Send Mails</a></li>
+                        <li><a href="pages_sudo_manage_incommingmails.php">Inbox</a></li>
+                    </ul>
+                
+                </li>
+                -->
+                <!--password resets-->
+                <!-- <li title="Password Resets">
+                    <a href="#">
+                        <span class="menu_icon"><i class="material-icons">lock</i></span>
+                        <span class="menu_title">Password Resets</span>
+                    </a>
+                    <ul>
+                        <li><a href="pages_sudo_manage_librarian_password_resets.php">Manage Librarians</a></li>
+                        <li><a href="pages_sudo_manage_student_password_resets.php">Manage Students</a></li>
+                    </ul>
+                </li> -->
+            </ul>
+        </div>
+    </aside>
